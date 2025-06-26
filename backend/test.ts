5afe06246b5eacf5111331cf591abfc333401d86
taty3385 <tatyy661@gmail.com>
2025-06-26 20:17:00 -0300

    agrego carpeta dist para poder deployar

--- backend/test.ts
+++
@@ -1,9 +0,0 @@
-import modelOrder from '../src/model/pedidos.Model';
-
-
-const id=1750611147697
-
-const deleteOrder= modelOrder.deleteOder(id)
-console.log( "pedido de eliminado", deleteOrder);
-
-
