3af8db6397daddaaf7fa0e263515839616f87b49
taty3385 <tatyy661@gmail.com>
2025-06-26 22:41:30 -0300

    .

--- backend/app.ts
+++ backend/app.ts
@@ -13,7 +13,7 @@ const PORT= 3000;
 app.use(cors())
 app.use(express.json())
 
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public', 'index.html'));
 });
