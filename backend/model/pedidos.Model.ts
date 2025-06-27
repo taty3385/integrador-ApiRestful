ef847771cf313480756323da4d6a05b20e12c373
taty3385 <tatyy661@gmail.com>
2025-06-27 16:31:49 -0300

    cambio ruta del modelo

--- backend/model/pedidos.Model.ts
+++ backend/model/pedidos.Model.ts
@@ -1,7 +1,7 @@
 import fs from "fs";
 import path from "path";
 
-const filePath = path.join(__dirname, "../data/pedidos.json");
+const filePath = path.resolve(__dirname, "../data/pedidos.json");
 
 
 const modelOrder = {
--- dist/model/pedidos.Model.js
+++ dist/model/pedidos.Model.js
@@ -5,7 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = __importDefault(require("fs"));
 const path_1 = __importDefault(require("path"));
-const filePath = path_1.default.join(__dirname, "../data/pedidos.json");
+const filePath = path_1.default.resolve(__dirname, "../data/pedidos.json");
 const modelOrder = {
     // leer archivo
     readOrder: () => {
