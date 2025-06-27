d30e2ea5505fb4c0d23aee5419f04294f2967e75
taty3385 <tatyy661@gmail.com>
2025-06-26 22:55:13 -0300

    .

---
+++ dist/app.js
@@ -0,0 +1,23 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const pedidosRoutes_1 = __importDefault(require("./routes/pedidosRoutes"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const path_1 = __importDefault(require("path"));
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+const PORT = 3000;
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+app.use(express_1.default.static(path_1.default.join(__dirname, 'public')));
+app.get('/', (req, res) => {
+    res.sendFile(path_1.default.join(__dirname, './public', 'index.html'));
+});
+app.use('/api', pedidosRoutes_1.default);
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el http://localhost:${PORT}`);
+});
